34cf7d9b13dbe558b877b02b018182599c210015
prtzl <matej.blagsic@protonmail.com>
2022-09-07 20:33:46 +0200

    Add helper function that creates flash script for a firmware

--- flake.nix
+++ flake.nix
@@ -7,8 +7,33 @@
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+
+    jlink = pkgs.callPackage ./default.nix {};
+
+    connection-type = { swd = "SWD"; jtag = "JTAG"; };
+    make-script = { device ? "", fpath ? "", speed ? 4000, addr ? "0x08000000", connection ? connection-type.swd }:
+      pkgs.writeTextFile {
+        name = "jlink-script";
+        text = ''
+          device ${device}
+          si ${connection}
+          speed ${builtins.toString speed}
+          loadfile ${fpath},${addr}
+          r
+          g
+          qc
+        '';
+    };
+
+    flash-script = script:
+      pkgs.writeShellApplication {
+        name = "flash-jlink";
+        text = "${jlink}/bin/JLinkExe -commanderscript ${script}";
+        runtimeInputs = [ jlink ];
+    };
   in
   {
-    defaultPackage.${system} = pkgs.callPackage ./default.nix {};
+    inherit jlink connection-type make-script flash-script;
+    defaultPackage.${system} = jlink;
   };
 }
